226a21f17eb9cc10faaf5f60c42f24cae831985a
unknown <ehurst01@gmail.com>
2018-05-06 15:11:14 -0700

    both list are showing handlesbars if logic working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Green Chilli", FALSE),("Black and Blu", FALSE),("Swiss and Shroom", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Green Chilli", FALSE),("Black and Blu", FALSE),("Swiss and Shroom", FALSE),("Cat",TRUE);
